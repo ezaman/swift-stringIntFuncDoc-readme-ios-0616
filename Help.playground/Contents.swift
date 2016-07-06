79c429fbc43fae4125465926e56eec6aa606b35c
ezaman <ehsanmzaman@gmail.com>
2016-07-05 23:28:11 -0400

    strings and methods introduction

--- Help.playground/Contents.swift
+++ Help.playground/Contents.swift
@@ -1,6 +1,9 @@
-let name = "Audrey"
+var name = "Audrey"
+var lastName = "Hepburn"
 name.uppercaseString
+name.lowercaseString
+
 
 let uppercaseName = name.uppercaseString
-print("\(name)... \(name)... \(uppercaseName)! WAKE UP!")
+print("\(name)... \(name)... \(uppercaseName)! WAKE UP NOW. TIME TO GO TO FLATIRON!")
 // prints "Audrey... Audrey... AUDREY! WAKE UP!")
